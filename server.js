bf80e378561af6806d2b26af0da95f290f83be28
GShock Im <gshock.im@nsuslab.com>
2018-12-27 12:36:04 +0900

    503

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
 
+
 http.createServer(function (req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.writeHead( 503, { 'Content-Type': 'application/json' });
+    res.end('{Result:503}');
     
 }).listen(process.env.PORT || 8080);
